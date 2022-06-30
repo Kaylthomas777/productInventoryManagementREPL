89be10e3249cfcfd4aa7338b6476aa6ff4f80c60
John Kayl Thomas <commander@arceus.attlocal.net>
2022-06-30 14:55:35 -0400

    Added unit test cases for validAddProduct

--- tests/prodInvMgmtService.test.js
+++ tests/prodInvMgmtService.test.js
@@ -1,7 +1,17 @@
 const ps = require("../lib/prodInvMgmtService");
 const pimServiceObj = Object.create(ps.PimService);
 
-test("Testing the test", () => {
-  pimServiceObj.addProduct("Product", 1);
-  expect(pimServiceObj.productCatalog[1]).toBe("Product");
+const validAddProductCases = [
+  [["Product1", "SKU1"], "Product1"],
+  [["Product2", "SKU2"], "Product2"],
+  [["Product3", "SKU3"], "Product3"],
+  [["Product4", "SKU4"], "Product4"],
+  [["Product5", "SKU5"], "Product5"],
+];
+
+test.each(validAddProductCases)("SHOULD add product", (product, expected) => {
+  let productName = product[0];
+  let sku = product[1];
+  pimServiceObj.addProduct(productName, sku);
+  expect(pimServiceObj.productCatalog[sku]).toBe(expected);
 });
